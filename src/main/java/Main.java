70d164bc2cd0c8aa580bcd46320d320716fa9325
Olga Kusakina <kusakina_olya@mail.ru>
2022-07-07 12:50:03 +0300

    birthday

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+            public static void main(String[] args) {
+
+
+                Post post = new Post();
+                post.birthday = new FormDate();
+
+                post.birthday.day = Integer.parseInt("13");
+                post.birthday.month = Integer.parseInt("6");
+                post.birthday.year = Integer.parseInt("1999");
+
+
+                post.name = "Иван";
+                post.passport = "4444 № 44444444";
+                post.patronymic = "Иванович";
+                post.phone ="+7 (999)-999-99-99";
+                post.surname = "Иванов";
+                post.subscription = true;
+
+
+                System.out.println(post.birthday.day);
+                System.out.println(post.birthday.month);
+                System.out.println(post.birthday.year);
+                System.out.println(post.name);
+                System.out.println(post.passport);
+                System.out.println(post.patronymic);
+                System.out.println(post.phone);
+                System.out.println(post.surname);
+                System.out.println(post.subscription);
+    }
+}
